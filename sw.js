c46e87a592bcae317733fab6a2607f0a6df0b0ae
iversonlin <jingjin31372011@gmail.com>
2020-03-14 17:45:40 +0800

    Site updated: 2020-03-14 17:45:36

--- sw.js
+++ sw.js
@@ -24,6 +24,8 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
+  precacheUrls.push('/2020/03/14/Facebook广告指引与实操/');
+
   precacheUrls.push('/2019/10/18/一张纸解决一个项目，去芜存菁直面OGSM/');
 
   precacheUrls.push('/2018/01/05/时间管理者的时间都去哪了？/');
@@ -31,8 +33,6 @@ var precacheUrls = [];
   precacheUrls.push('/2019/09/07/深入浅出Facebook B2B 客户开发（入门）/');
 
   precacheUrls.push('/2019/12/08/独立站，商家的累赘还是牌面？/');
-
-  precacheUrls.push('/2019/10/13/运营，从目标管理开始/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
